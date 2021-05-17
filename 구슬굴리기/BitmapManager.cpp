7ac8f88f00926715c0d477a5da998d2431666ebf
lja3723 <lja3723@gmail.com>
2021-05-17 22:38:02 +0900

    return to no bug world2

--- 구슬굴리기/BitmapManager.cpp
+++ 구슬굴리기/BitmapManager.cpp
@@ -4,16 +4,10 @@ using namespace RollingBall;
 
 int BitmapManager::object_count = 0;
 HINSTANCE BitmapManager::hInstance = NULL;
-HWND BitmapManager::hwnd = NULL;
 HBITMAP BitmapManager::floor = NULL;
 HBITMAP BitmapManager::ball[BallSizeCount] = { NULL, };
 HBITMAP BitmapManager::ball_mask[BallSizeCount] = { NULL, };
 
-BitmapManager::BitmapManager()
-{
-	hDCwindowBuffer = NULL;
-	set_BallSizeType(BallSize_medium);
-}
 BitmapManager::~BitmapManager()
 {
 	if (object_count == 0)
@@ -26,18 +20,15 @@ BitmapManager::~BitmapManager()
 			DeleteObject(ball_mask[size]);
 		}
 	}
-	release_hDCwindowBuffer();
 	object_count--;
 }
 
-
-void BitmapManager::initialize(HINSTANCE m_hInstance, HWND m_hwnd, int m_BallSizeType)
+void BitmapManager::initialize(HINSTANCE m_hInstance, int m_BallSizeType)
 {
 	if (object_count == 0)
 	{
 		//∫Ò∆Æ∏  ∑ŒµÂ
 		hInstance = m_hInstance;
-		hwnd = m_hwnd;
 		floor = LoadBitmap(hInstance, MAKEINTRESOURCE(IDB_BITMAP_FLOOR));
 		for (int size = 0; size < BallSizeCount; size++)
 		{
@@ -46,63 +37,34 @@ void BitmapManager::initialize(HINSTANCE m_hInstance, HWND m_hwnd, int m_BallSiz
 		}
 	}
 	set_BallSizeType(m_BallSizeType);
-	set_hDCwindowBuffer();
 
 	object_count++;
 }
-BOOL BitmapManager::isInitialized()
-{
-	return object_count > 0;
-}
-
 
 void BitmapManager::set_BallSizeType(int m_BallSizeType)
 {
 	BallSizeType = m_BallSizeType;
 }
-void BitmapManager::set_hDCwindowBuffer()
-{
-	if (hDCwindowBuffer != NULL) 
-		release_hDCwindowBuffer();
-	RECT rt;
-	HDC hdc = GetDC(hwnd);
-
-	GetClientRect(hwnd, &rt);
-	hDCwindowBuffer = CreateCompatibleBitmap(hdc, rt.right, rt.bottom);
-	ReleaseDC(hwnd, hdc);
-}
-
 
 HBITMAP BitmapManager::get_hBitmap_floor()
 {
 	return floor;
 }
+
 HBITMAP BitmapManager::get_hBitmap_ball()
 {
 	return ball[BallSize_toIdx(BallSizeType)];
 }
+
 HBITMAP BitmapManager::get_hBitmap_ball_mask()
 {
 	return ball_mask[BallSize_toIdx(BallSizeType)];
 }
-HBITMAP BitmapManager::get_hBitmap_hDCwindowBuffer()
-{
-	return hDCwindowBuffer;
-}
+
 int BitmapManager::get_BallSizeType()
 {
 	return BallSizeType;
 }
-
-
-void BitmapManager::release_hDCwindowBuffer()
-{
-	if (hDCwindowBuffer == NULL) return;
-
-	DeleteObject(hDCwindowBuffer);
-	hDCwindowBuffer = NULL;
-}
-
 
 int BitmapManager::BallSize_toIdx(int BallSize)
 {
