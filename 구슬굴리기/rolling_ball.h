2539f90709cd7127c046a8663f26b6aec7739520
lja3723 <lja3723@gmail.com>
2021-05-29 12:59:03 +0900

    update관련 함수를 private로 수정

--- 구슬굴리기/rolling_ball.h
+++ 구슬굴리기/rolling_ball.h
@@ -24,12 +24,13 @@ namespace RollingBall
 		Controller controller;
 		vector<Ball> ball;
 
+		void update_window();
+		void update_state();
 	public:
 		//RollingBallClass 변수를 사용하기 전 반드시 수행해야 함
 		BOOL init(HINSTANCE m_hInstance, HWND m_hwnd);
 
-		void update_window();
-		void update_state();
+		//윈도우 이벤트를 rollingBall프로그램이 받아들임
 		void send_windowEvent(UINT m_iMsg, WPARAM m_wParam, LPARAM m_lParam);
 	};
 }
