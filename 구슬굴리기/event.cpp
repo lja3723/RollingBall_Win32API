98188c7a979286cde2532fb0d6010ed535cd652a
lja3723 <lja3723@gmail.com>
2021-06-03 23:46:03 +0900

    event 파일 기본구조 작성

--- 구슬굴리기/event.cpp
+++ 구슬굴리기/event.cpp
@@ -1,4 +1,106 @@
 ﻿#include "event.h"
-
+#include <algorithm>
 
 using namespace RollingBall;
+using std::find;
+using std::sort;
+
+
+/////////////////////////
+// 
+//	EventProducer class
+// 
+/////////////////////////
+MouseEvent RollingBall::EventProducer::produce_mouseEvent(UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	MouseEvent e;
+	memset(&e, 0, sizeof(e));
+
+	switch (iMsg)
+	{
+	case WM_LBUTTONDOWN:
+	case WM_LBUTTONUP:
+	case WM_LBUTTONDBLCLK:
+	case WM_MOUSEMOVE:
+	case WM_RBUTTONDOWN:
+	case WM_RBUTTONUP:
+	case WM_RBUTTONDBLCLK:
+	case WM_MBUTTONDOWN:
+	case WM_MBUTTONUP:
+	case WM_MBUTTONDBLCLK:
+	case WM_MOUSEWHEEL:
+		break;
+	}
+
+	return e;
+}
+KeyboardEvent RollingBall::EventProducer::produce_keyboardEvent(UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	KeyboardEvent e;
+	switch (iMsg)
+	{
+	case WM_KEYDOWN:
+	case WM_KEYUP:
+		break;
+	}
+
+	return e;
+}
+void RollingBall::EventProducer::translate_windowEvent(UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (iMsg)
+	{
+	case WM_LBUTTONDOWN:
+	case WM_LBUTTONUP:
+	case WM_LBUTTONDBLCLK:
+	case WM_MOUSEMOVE:
+	case WM_RBUTTONDOWN:
+	case WM_RBUTTONUP:
+	case WM_RBUTTONDBLCLK:
+	case WM_MBUTTONDOWN:
+	case WM_MBUTTONUP:
+	case WM_MBUTTONDBLCLK:
+	case WM_MOUSEWHEEL:
+		{
+			MouseEvent e = produce_mouseEvent(iMsg, wParam, lParam);
+
+			//마우스 좌표에 존재하는 모든 EventAcceptable 객체에 마우스 이벤트를 보낸다
+			// -> 나중에 우선순위를 참고해 하나의 객체에 이벤트를 보내게 수정할 것임
+			for (int i = 0; i < EventAcceptable::object_ref.size(); i++)
+				if (EventAcceptable::object_ref[i]->isObjectArea(e.pos))
+					EventAcceptable::object_ref[i]->event_mouse(e);
+		}
+		return; 
+
+	case WM_KEYDOWN:
+	case WM_KEYUP:
+		{
+			KeyboardEvent e = produce_keyboardEvent(iMsg, wParam, lParam);
+			//모든 EventAcceptable 객체에 키보드 이벤트를 보낸다
+			for (int i = 0; i < EventAcceptable::object_ref.size(); i++)
+				EventAcceptable::object_ref[i]->event_keyboard(e);
+		}
+		return;
+	}
+}
+
+
+/////////////////////////////
+// 
+//	EventAcceptable class
+// 
+/////////////////////////////
+int EventAcceptable::object_count = 0;
+vector<EventAcceptable*> EventAcceptable::object_ref = vector<EventAcceptable*>();
+
+RollingBall::EventAcceptable::EventAcceptable()
+{
+	object_ref.push_back(this);
+}
+
+RollingBall::EventAcceptable::~EventAcceptable()
+{
+	auto iter = find(object_ref.begin(), object_ref.end(), this);
+	if (iter != object_ref.end())
+		object_ref.erase(iter);
+}
--- 구슬굴리기/event.h
+++ 구슬굴리기/event.h
@@ -1,10 +1,84 @@
 ﻿#ifndef __event_h__
 #define __event_h__
 
+#include <vector>
+#include <Windows.h>
+
+using std::vector;
 
 namespace RollingBall
 {
+	/*
+	* WM 메세지를 재해석해 사용하기 편하게 가공한 이벤트 추상 객체
+	*/
+	class Event abstract
+	{
 
+	};
+
+
+
+	/*
+	* 마우스 이벤트가 발생하면 그와 관련된 정보를 전달할 이벤트 객체
+	*/
+	class MouseEvent : Event
+	{
+	public:
+		POINT pos;
+	};
+
+
+
+	/*
+	* 키보드 이벤트가 발생하면 그와 관련된 정보를 전달할 이벤트 객체
+	*/
+	class KeyboardEvent : Event
+	{
+
+	};
+
+
+
+	/*
+	* WinAPI의 WM 메세지를 이벤트 객체로 해석한 뒤,
+	* 이벤트 수용가능 객체에 이벤트를 전달하는 클래스
+	*/
+	class EventProducer
+	{
+	private:
+		static MouseEvent produce_mouseEvent(UINT iMsg, WPARAM wParam, LPARAM lParam);
+		static KeyboardEvent produce_keyboardEvent(UINT iMsg, WPARAM wParam, LPARAM lParam);
+	public:
+		static void translate_windowEvent(UINT iMsg, WPARAM wParam, LPARAM lParam);
+	};
+
+
+
+	/*
+	* 이벤트를 처리할 수 있는 능력을 부여하는 인터페이스 클래스
+	* 이벤트를 처리하기 위해서는 가상 함수들을 반드시 구현해야 함
+	*/
+	class EventAcceptable
+	{
+		friend class EventProducer;
+	private:
+		static int object_count;
+		static vector<EventAcceptable*> object_ref;
+	protected:
+
+	public:
+		EventAcceptable();
+		~EventAcceptable();
+
+		virtual void event_mouse(MouseEvent e) abstract;
+		virtual void event_keyboard(KeyboardEvent e) abstract;
+		virtual BOOL isObjectArea(POINT pos) abstract;
+
+		//이벤트 우선순위를 결정해주는 함수
+		//this가 우선순위이면 음수, 동일 우선순위이면 0, 
+		//object가 우선순위이면 양수를 반환
+		virtual BOOL event_priority(EventAcceptable& object) abstract;
+	};
 }
 
 
