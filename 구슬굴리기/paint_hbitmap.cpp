a668b38b08a6693a2336d7637d177672a43e9897
lja3723 <lja3723@gmail.com>
2021-06-19 15:47:11 +0900

    일단 다시 분리전으로 돌리고 paint_hbitmap 소스만 되살려보기

--- 구슬굴리기/paint_hbitmap.cpp
+++ 구슬굴리기/paint_hbitmap.cpp
@@ -1,3 +1,146 @@
 ﻿#include "paint_hbitmap.h"
 
 using namespace RollingBall;
+
+Bitmap Paint_hBitmap::m_bmp = Bitmap();
+
+BOOL Paint_hBitmap::_windowBuffer::isSet()
+{
+	return m_windowBuffer != NULL;
+}
+void Paint_hBitmap::_windowBuffer::init()
+{
+	m_windowBuffer = NULL;
+}
+void RollingBall::Paint_hBitmap::_windowBuffer::set(const HWND& hwnd, const HDC& hDC_window)
+{
+	if (isSet())
+		release();
+
+	RECT windowRect;
+	//화면 DC와 호환되는 hBitmap을 로드한다
+	GetClientRect(hwnd, &windowRect);
+	m_windowBuffer = m_bmp.create_hDC_compatible(hDC_window, windowRect);
+}
+void RollingBall::Paint_hBitmap::_windowBuffer::release()
+{
+	if (!isSet()) return;
+	//hBitmap을 삭제함
+	m_bmp.delete_hDC_compatible(m_windowBuffer);
+	init();
+}
+
+BOOL Paint_hBitmap::_windowBuffer::_old::isBackedUp()
+{
+	return m_windowBuffer != NULL;
+}
+void Paint_hBitmap::_windowBuffer::_old::init()
+{
+	m_windowBuffer = NULL;
+}
+void Paint_hBitmap::_windowBuffer::_old::backup(Paint_hDC& hDC)
+{
+	if (!hDC.mem.windowBuffer.isSet()) return;
+	if (isBackedUp())
+		rollback(hDC);
+
+	m_windowBuffer
+		= (HBITMAP)SelectObject(
+			hDC.mem.windowBuffer,
+			m_windowBuffer
+		);
+}
+void Paint_hBitmap::_windowBuffer::_old::rollback(Paint_hDC& hDC)
+{
+	if (!isBackedUp()) return;
+	SelectObject(
+		hDC.mem.windowBuffer,
+		m_windowBuffer
+	);
+	init();
+}
+
+BOOL Paint_hBitmap::_res::isSet()
+{
+	return flag_isSet;
+}
+void Paint_hBitmap::_res::init()
+{
+	for (int i = 0; i < m_res.size(); i++)
+		m_res[i] = NULL;
+
+	//아래 플래그 변수는 삭제할 수 있을 것 같다 
+	//(resource.size() != 0)
+	flag_isSet = FALSE;
+}
+void RollingBall::Paint_hBitmap::_res::set()
+{
+	for (int i = 0; i < m_res.size(); i++)
+		m_res[i] = m_bmp(i);
+	flag_isSet = TRUE;
+}
+void Paint_hBitmap::_res::resize(const size_t& newSize)
+{
+	m_res.resize(newSize);
+}
+
+BOOL Paint_hBitmap::_res::_old::isBackedUp()
+{
+	return flag_isBackedUp;
+}
+void Paint_hBitmap::_res::_old::resize(const size_t& newSize)
+{
+	m_res.resize(newSize);
+}
+void Paint_hBitmap::_res::_old::init()
+{
+	for (int i = 0; i < m_res.size(); i++)
+		m_res[i] = NULL;
+}
+void Paint_hBitmap::_res::_old::backup(Paint_hDC& hDC)
+{
+	if (!hDC.mem.res.isSet()) return;
+	if (!isBackedUp())
+		rollback(hDC);
+
+	for (int i = 0; i < m_res.size(); i++)
+		m_res[i]
+		= (HBITMAP)SelectObject(
+			hDC.mem.res[i],
+			m_res[i]
+		);
+
+	flag_isBackedUp = TRUE;
+}
+void Paint_hBitmap::_res::_old::rollback(Paint_hDC& hDC)
+{
+	if (!isBackedUp()) return;
+
+	for (int i = 0; i < m_res.size(); i++)
+		SelectObject(
+			hDC.mem.res[i],
+			m_res[i]
+		);
+
+	init();
+
+	flag_isBackedUp = FALSE;
+}
+
+BOOL RollingBall::Paint_hBitmap::init(HINSTANCE hInstance)
+{
+	return m_bmp.init(hInstance);
+}
+BOOL RollingBall::Paint_hBitmap::isInit()
+{
+	return m_bmp.isInit();
+}
+int RollingBall::Paint_hBitmap::bmpidx(Object& object, Scaler& scale, BOOL mask_texture)
+{
+	return m_bmp.idx(object, scale, mask_texture);
+}
+int RollingBall::Paint_hBitmap::res_count()
+{
+	if (!m_bmp.isInit()) return 0;
+	return m_bmp.file_count();
+}
--- 구슬굴리기/paint_hbitmap.h
+++ 구슬굴리기/paint_hbitmap.h
@@ -1,12 +1,71 @@
 ﻿#ifndef __paint_hbitmap_h__
 #define __paint_hbitmap_h__
 
+#include <Windows.h>
+#include "bitmap.h"
+#include "paint_hdc.h"
+
 namespace RollingBall
 {
+	class Paint_hBitmap
+	{
+	private:
+		static Bitmap m_bmp;
+	public:
+		class _windowBuffer
+		{
+		private:
+			HBITMAP m_windowBuffer;
+		public:
+			BOOL isSet();
+			void init();
+			void set(const HWND& hwnd, const HDC& hDC_window);
+			void release();
 
+			class _old
+			{
+			private:
+				HBITMAP m_windowBuffer;
+			public:
+				BOOL isBackedUp();
+				void init();
+				void backup(Paint_hDC& hDC);
+				void rollback(Paint_hDC& hDC);
+			} old;
 
+		} windowBuffer;
 
+		class _res
+		{
+		private:
+			vector<HBITMAP> m_res;
+			BOOL flag_isSet;
+		public:
+			BOOL isSet();
+			void init();
+			void set();
+			void resize(const size_t& newSize);
+
+			class _old
+			{
+			private:
+				vector<HBITMAP> m_res;
+				BOOL flag_isBackedUp;
+			public:
+				BOOL isBackedUp();
+				void init();
+				void backup(Paint_hDC& hDC);
+				void rollback(Paint_hDC& hDC);
+				void resize(const size_t& newSize);
+			} old;
+		} res;
+
+	public:
+		BOOL init(HINSTANCE hInstance);
+		BOOL isInit();
+		int bmpidx(Object& object, Scaler& scale, BOOL mask_texture = FALSE);
+		int res_count();
+	};
 }
-
 
 #endif
